db23f980dfa99a137c2fb827037bbf16a5916cb3
Cvetan Dimeski <dimeski.cvetan@gmail.com>
2022-03-23 19:50:35 +0100

    some random change

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
+function required() {
+    throw Error('Field is required');
+    return;
+}
 console.log('hello from js', add(2,3));
 
-function add(x,y) {
+function add(x = required(),y = requred()) {
+    if(x == null) {
+        throw Error('x cannot be added');
+    }
     return x+y;
 }
